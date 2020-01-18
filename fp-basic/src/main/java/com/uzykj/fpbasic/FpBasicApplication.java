92d759a000b2e62e7f6b0acda072cc9211a3e1c7
ghostxbh <ghostxbh@hotmail.com>
2020-01-18 15:08:15 +0800

    add fp-basic module

---
+++ fp-basic/src/main/java/com/uzykj/fpbasic/FpBasicApplication.java
@@ -0,0 +1,13 @@
+package com.uzykj.fpbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpBasicApplication.class, args);
+	}
+
+}
---
+++ fp-basic/src/test/java/com/uzykj/fpbasic/FpBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzykj.fpbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
