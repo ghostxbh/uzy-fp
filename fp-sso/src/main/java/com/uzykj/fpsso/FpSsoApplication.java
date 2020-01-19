de43189c2d980eb5b594b04bb00adf77347541bc
ghostxbh <ghostxbh@hotmail.com>
2020-01-19 10:36:47 +0800

    add fp-sso module

---
+++ fp-sso/src/main/java/com/uzykj/fpsso/FpSsoApplication.java
@@ -0,0 +1,13 @@
+package com.uzykj.fpsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpSsoApplication.class, args);
+    }
+
+}
