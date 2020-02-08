3b9ce0a4c4d01b0ae6dc7c129128be6f202cbaf2
ghostxbh <ghostxbh@hotmail.com>
2020-02-09 00:35:11 +0800

    add MD5、AES、Response utils

---
+++ fp-common/src/main/java/com/uzykj/fpcommon/utils/MD5Util.java
@@ -0,0 +1,34 @@
+package com.uzykj.fpcommon.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author ghostxbh
+ * @date 2020/2/8
+ * @description MD5加密工具
+ */
+public class MD5Util {
+    public static String md5(String code) {
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+        byte[] digest = md.digest(code.getBytes());
+        return byte2hex(digest);
+    }
+
+    private static String byte2hex(byte[] b) {
+        StringBuilder sb = new StringBuilder();
+        for (int n = 0; n < b.length; ++n) {
+            String stmp = Integer.toHexString(b[n] & 0xFF);
+            if (stmp.length() == 1) {
+                sb.append("0");
+            }
+            sb.append(stmp);
+        }
+        return sb.toString().toUpperCase();
+    }
+}
---
+++ fp-common/src/main/java/com/uzykj/fpcommon/utils/ResponseUtil.java
@@ -0,0 +1,45 @@
+package com.uzykj.fpcommon.utils;
+
+import com.tongwei.common.model.ResultCode;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author yangz
+ * @date 2018年2月6日 下午7:24:23
+ * @description 响应工具类
+ */
+public class ResponseUtil {
+
+    private static final String ForbiddenJsonMsg = "{\"errorCode\":"+ResultCode.SSO_LOGIN_REJECTED_ERROR+",\"msg\":\"您被禁止登录!\"}";
+
+    private static final String UnAuthorizedJsonMsg = "{\"errorCode\":"+ResultCode.SSO_PERMISSION_ERROR+",\"msg\":\"您没有访问权限!\"}";
+
+    public static void responseJson(HttpServletResponse resp, String msg) {
+        PrintWriter writer = null;
+        try {
+            resp.setCharacterEncoding("utf-8");
+            resp.setContentType("application/json; charset=utf-8");
+            resp.setStatus(200);
+            writer = resp.getWriter();
+            writer.print(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+
+    public static void responseUnAuthorizedJson(HttpServletResponse resp) {
+        responseJson(resp, UnAuthorizedJsonMsg);
+    }
+
+    public static void responseForbiddenJson(HttpServletResponse resp) {
+        responseJson(resp, ForbiddenJsonMsg);
+    }
+
+}
