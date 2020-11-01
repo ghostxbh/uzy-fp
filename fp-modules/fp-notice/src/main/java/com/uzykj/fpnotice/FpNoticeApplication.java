983c170936189608c1a7c5feb92b3347341af82f
ghostxbh <ghostxbh@hotmail.com>
2020-11-01 23:17:10 +0800

    add notice app

---
+++ fp-modules/fp-notice/src/main/java/com/uzykj/fpnotice/FpNoticeApplication.java
@@ -0,0 +1,13 @@
+package com.uzykj.fpnotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpNoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpNoticeApplication.class, args);
+    }
+
+}
