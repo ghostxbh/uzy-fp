60388a695b70379d2aadbc45b00956ffa328d6b0
ghostxbh <ghostxbh@hotmail.com>
2021-11-07 19:36:07 +0800

    refator: update mailConstant

--- fp-system/src/main/java/com/uzykj/system/constant/MailConstant.java
+++ fp-system/src/main/java/com/uzykj/system/constant/MailConstant.java
@@ -9,16 +9,5 @@ package com.uzykj.system.constant;
  * @Version 1.0
  */
 public class MailConstant {
-    /**
-     * 发送状态
-     */
-    public static final Integer MAIL_SEND_STATUS_TOBE = 0;
-    public static final Integer MAIL_SEND_STATUS_SUCCESS = 1;
-    public static final Integer MAIL_SEND_STATUS_FAIL = -1;
 
-    /**
-     * 发件类型
-     */
-    public static final Integer MAIL_SEND_TYPE_BATCH = 1;
-    public static final Integer MAIL_SEND_TYPE_SINGLE = 2;
 }
