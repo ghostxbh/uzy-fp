e3f51a070ab672274d3b4801f6d4844ac1674917
ghostxbh <ghostxbh@hotmail.com>
2021-11-09 14:58:00 +0800

    fix: 增加get请求

--- fp-admin/src/main/java/com/uzykj/admin/web/MailController.java
+++ fp-admin/src/main/java/com/uzykj/admin/web/MailController.java
@@ -27,9 +27,32 @@ public class MailController {
     private MailSenderService mailSenderService;
 
     @PostMapping("/send")
-    public R sender(@RequestParam MailBatchLog mailBatchLog) {
+    public R sendOfPost(@RequestBody MailBatchLog mailBatchLog) {
         log.info("sender param: " + mailBatchLog.toString());
         try {
+            if (ObjectUtils.isEmpty(mailBatchLog.getUserId())
+                    || ObjectUtils.isEmpty(mailBatchLog.getPropertiesId())
+                    || ObjectUtils.isEmpty(mailBatchLog.getReceiveAddress())) {
+                return R.failed("缺少必填参数");
+            }
+
+            int sendType = mailBatchLog.getReceiveAddress().contains(";") ? MailSendType.BATCH.getCode() : MailSendType.SINGLE.getCode();
+            mailBatchLog.setSendType(sendType);
+            mailBatchLog.setStatus(MailSendStatus.TOBE.getCode());
+
+            mailSenderService.realSender(mailBatchLog);
+            return R.ok(null);
+        } catch (Exception e) {
+            log.error("mail sender error:", e);
+            return R.failed("mail sender error");
+        }
+    }
+
+    @GetMapping("/send")
+    public R sendOfGet(@RequestParam String mailParams) {
+        log.info("sender param: " + mailParams);
+        try {
+            MailBatchLog mailBatchLog = JSONObject.parseObject(mailParams, MailBatchLog.class);
             if (ObjectUtils.isEmpty(mailBatchLog.getUserId())
                     || ObjectUtils.isEmpty(mailBatchLog.getPropertiesId())
                     || ObjectUtils.isEmpty(mailBatchLog.getReceiveAddress())) {
