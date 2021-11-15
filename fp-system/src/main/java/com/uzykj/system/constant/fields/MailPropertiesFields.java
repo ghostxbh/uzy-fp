64c9624fb8f05ed6d84f20079eff2531b7bc9f62
ghostxbh <ghostxbh@hotmail.com>
2021-11-15 18:18:30 +0800

    fix: 处理字段信息与swagger和springboot版本兼容问题

--- fp-system/src/main/java/com/uzykj/system/constant/fields/MailPropertiesFields.java
+++ fp-system/src/main/java/com/uzykj/system/constant/fields/MailPropertiesFields.java
@@ -13,11 +13,11 @@ public class MailPropertiesFields {
     public static final String USER_ID = "user_id";
     public static final String COMPANY_ID = "company_id";
     public static final String BUSINESS_ID = "business_id";
-    public static final String KEY = "key";
-    public static final String HOST = "host";
-    public static final String PORT = "port";
-    public static final String USERNAME = "username";
-    public static final String PASSWORD = "password";
+    public static final String KEY = "mail_key";
+    public static final String HOST = "mail_host";
+    public static final String PORT = "mail_port";
+    public static final String USERNAME = "mail_user";
+    public static final String PASSWORD = "mail_pwd";
     public static final String PROTOCOL = "protocol";
     public static final String DEFAULT_ENCODING = "default_encoding";
     public static final String UPDATOR = "updator";
